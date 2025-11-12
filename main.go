5110913e7e63bfa5cfc87154f0327f34183e1810
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-11-12 13:33:42 +0000

    style the protocol input

--- main.go
+++ main.go
@@ -3,29 +3,45 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"image/color"
 	"log"
 )
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Hello")
+	myWindow := myApp.NewWindow("PuppySniffer")
+	myWindow.Resize(fyne.NewSize(1280, 720))
 
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Enter the Protocol...(Ex:Ip)")
 
-	content := readUserInput(input)
+	content := readUserInput("Enter the Protocol...(Ex:Ip)")
 
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 }
 
-func readUserInput(input *widget.Entry) *fyne.Container {
-	content := container.NewVBox(input, widget.NewButton("Save", func() {
+func readUserInput(placeholder string) *fyne.Container {
+	styledInputContainer, input := StyledProtocolInput(placeholder)
+
+	content := container.NewVBox(styledInputContainer, widget.NewButton("Save", func() {
 		log.Println("Content was:", input.Text)
 		input.SetText("")
 	}))
-
 	return content
+}
+
+func StyledProtocolInput(placeholder string) (*fyne.Container, *widget.Entry) {
+	input := widget.NewEntry()
+	input.SetPlaceHolder(placeholder)
+
+	rect := canvas.NewRectangle(color.RGBA{168, 187, 163, 255})
+	rect.CornerRadius = 5
+
+	content := container.NewPadded(input)
+
+	return container.NewStack(rect, content), input
 }
