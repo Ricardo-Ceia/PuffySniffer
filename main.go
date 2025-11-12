7216d25daf75ad1040611df2ff92de5fd79901cf
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-11-12 00:08:45 +0000

    create a clock function

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
+	"image/color"
+	"time"
 )
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("PuppySniffer")
-	hello := widget.NewLabel("Hello Fyne!")
-	myWindow.SetContent(container.NewVBox(hello))
+	myWindow := myApp.NewWindow("Hello")
+	output := canvas.NewText(time.Now().Format(time.TimeOnly), color.NRGBA{G: 0xff, A: 0xff})
+	output.TextStyle.Monospace = true
+	output.TextSize = 32
+	myWindow.SetContent(output)
+
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for range ticker.C {
+			fyne.Do(func() {
+				output.Text = time.Now().Format(time.TimeOnly)
+				output.Refresh()
+			})
+		}
+	}()
 	myWindow.ShowAndRun()
 }
